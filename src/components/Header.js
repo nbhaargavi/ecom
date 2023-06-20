2ebfb65010d7f15bed3d98d9662d9135b2bdb2d1
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-06-20 15:23:56 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="block row center">
       <div>
-          <h1>BOOK STORE</h1>
+          <h1>BOOK</h1>
       </div>
     </header>
   );
